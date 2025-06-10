ed01566946314a5d6cfe7f511a480fdb4f79036d
Harsh <hp424805@gmail.com>
2025-06-10 17:07:55 +0530

    chore: remove hardcoded BACKEND_URL

--- apps/web/lib/utils.ts
+++ apps/web/lib/utils.ts
@@ -4,5 +4,3 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
 	return twMerge(clsx(inputs));
 }
-
-export const BACKEND_URL = process.env.BACKEND_URL || "http://localhost:1284";
